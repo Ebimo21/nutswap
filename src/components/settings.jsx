c4b0e081333be6c50ddc87fb4e0202bb47f6fce3
Pondei Ebimobowei <52638069+Ebimo21@users.noreply.github.com>
2024-07-23 07:32:46 +0100

    add border to boxes

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -13,12 +13,12 @@ export default function Settings ({ stopFn, closeSettings, cardOpen }){
         <p className="text-sm text-[#ffffff7b]">Your transaction will revert if the price changes unfavourably by more than this percentage</p>
 
         <div className="mt-5 flex flex-wrap items-center justify-between gap-x-2 gap-y-4">
-            <SlippageBadge fn={handleSetSlippage} style={` ${slippage == 1 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)] ": ""} w-24 text-sm py-2 bg-transparent `} amount={1} />
-            <SlippageBadge fn={handleSetSlippage} id="five" style={` ${slippage == 5 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)]": ""} w-24 text-sm py-2 bg-transparent`} amount={5} />
-            <SlippageBadge fn={handleSetSlippage} style={` ${slippage == 10 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)]": ""} w-24 text-sm py-2 bg-transparent`} amount={10} />
+            <SlippageBadge fn={handleSetSlippage} style={` ${slippage == 1 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)] ": "border-2 border-[#1b1b1a]"} w-24 text-sm py-2 bg-transparent `} amount={1} />
+            <SlippageBadge fn={handleSetSlippage} id="five" style={` ${slippage == 5 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)]": "border-2 border-[#1b1b1a]"} w-24 text-sm py-2 bg-transparent`} amount={5} />
+            <SlippageBadge fn={handleSetSlippage} style={` ${slippage == 10 ? "border-[rgba(255,255,108,0.5)] border text-[rgba(255,255,108,0.5)]": "border-2 border-[#1b1b1a]"} w-24 text-sm py-2 bg-transparent`} amount={10} />
             <SlippageInputBadge 
               setSlippage={handleSetSlippage} 
-              style={` ${slippage == "Custom %" ? "": ""} w-full bg-transparent text-[#ffff6c] text-xl `} 
+              style={` ${slippage == "Custom %" ? "": ""} w-full text-[#ffff6c] text-xl `} 
               slippage={slippage} />
         </div>
 
